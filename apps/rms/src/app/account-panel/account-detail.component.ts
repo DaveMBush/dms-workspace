0308c2bec0455e739205ae347a3048de1bb1fe41
Dave Bush <dave@davembush.com>
2025-06-25 14:57:22 -0400

    Account Panel toolbar

---
+++ apps/rms/src/app/account-panel/account-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-account-detail',
+  standalone: true,
+  imports: [CommonModule],
+  template: 'Account Detail Panel',
+})
+export class AccountDetailComponent {}
--- apps/rms/src/app/account-panel/account-panel.component.ts
+++ apps/rms/src/app/account-panel/account-panel.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ActivatedRoute } from '@angular/router';
+import { AccountDetailComponent } from './account-detail.component';
+import { selectAccounts } from '../accounts/store/accounts/account.selectors';
+import { Account as AccountInterface } from '../accounts/store/accounts/account.interface';
+import { SmartArray } from '@smarttools/smart-signals';
+import { Top } from '../store/top/top.interface';
 
 @Component({
   selector: 'app-account-panel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ToolbarModule, AccountDetailComponent],
   templateUrl: './account-panel.component.html',
   styleUrls: ['./account-panel.component.scss']
 })
-export class AccountPanelComponent {}
+export class AccountPanelComponent {
+  private route = inject(ActivatedRoute);
+  private accounts$ = selectAccounts;
+
+  accountId = signal('');
+  accountName$ = computed(() => {
+    const id = this.accountId();
+    const accounts = this.accounts$();
+    for (let i = 0; i < accounts.length; i++) {
+      if (accounts[i].id === id) {
+        return accounts[i].name;
+      }
+    }
+    return 'Account Not Found';
+  });
+
+  constructor() {
+    this.route.params.subscribe(params => {
+      this.accountId.set(params['accountId']);
+    });
+  }
+}
