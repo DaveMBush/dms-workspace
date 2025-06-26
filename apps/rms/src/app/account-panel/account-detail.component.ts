b5165720b6035aed35e7a1a18357a7d215d07c6c
Dave Bush <dave@davembush.com>
2025-06-26 13:39:54 -0400

    start of new position modal

--- apps/rms/src/app/account-panel/account-detail.component.ts
+++ apps/rms/src/app/account-panel/account-detail.component.ts
@@ -4,18 +4,30 @@ import { TabViewModule } from 'primeng/tabview';
 import { RouterModule, Router, ActivatedRoute } from '@angular/router';
 import { TooltipModule } from 'primeng/tooltip';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { NewPositionComponent } from './new-position/new-position.component';
 
 @Component({
   selector: 'app-account-detail',
   standalone: true,
-  imports: [CommonModule, TabViewModule, RouterModule, TooltipModule, ButtonModule],
+  imports: [CommonModule, TabViewModule, RouterModule, TooltipModule, ButtonModule, DialogModule, NewPositionComponent],
   templateUrl: './account-detail.component.html',
   styleUrls: ['./account-detail.component.scss']
 })
 export class AccountDetailComponent {
+  showNewPositionDialog = false;
+
   constructor(public router: Router, public route: ActivatedRoute) {}
 
   get accountId(): string | null {
     return this.route.snapshot.paramMap.get('accountId');
+  }
+
+  openNewPositionDialog() {
+    this.showNewPositionDialog = true;
+  }
+
+  closeNewPositionDialog() {
+    this.showNewPositionDialog = false;
   }
 }
---
+++ apps/rms/src/app/account-panel/new-position/new-position.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-position',
+  standalone: true,
+  templateUrl: './new-position.component.html',
+  styleUrls: ['./new-position.component.scss']
+})
+export class NewPositionComponent {}
