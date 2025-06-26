0f12b8d960a683442e85877db26bbdfc729c5596
Dave Bush <dave@davembush.com>
2025-06-26 12:48:39 -0400

    add new position button

--- apps/rms/src/app/account-panel/account-detail.component.ts
+++ apps/rms/src/app/account-panel/account-detail.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabViewModule } from 'primeng/tabview';
 import { RouterModule, Router, ActivatedRoute } from '@angular/router';
+import { TooltipModule } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-account-detail',
   standalone: true,
-  imports: [CommonModule, TabViewModule, RouterModule],
+  imports: [CommonModule, TabViewModule, RouterModule, TooltipModule, ButtonModule],
   templateUrl: './account-detail.component.html',
   styleUrls: ['./account-detail.component.scss']
 })
 export class AccountDetailComponent {
   constructor(public router: Router, public route: ActivatedRoute) {}
+
+  get accountId(): string | null {
+    return this.route.snapshot.paramMap.get('accountId');
+  }
 }
