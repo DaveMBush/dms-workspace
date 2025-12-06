a15c2c1ae0e50173d92826e91c3a2bc89df41ef4
Dave Bush <dave@davembush.com>
2025-12-06 18:02:14 -0500

    format

--- apps/rms-material-e2e/playwright.config.ts
+++ apps/rms-material-e2e/playwright.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'DATABASE_URL="file:./database.db" node dist/apps/server/main.js',
+      command:
+        'DATABASE_URL="file:./database.db" node dist/apps/server/main.js',
       url: 'http://localhost:3000/health',
       reuseExistingServer: !process.env.CI,
       cwd: workspaceRoot,
