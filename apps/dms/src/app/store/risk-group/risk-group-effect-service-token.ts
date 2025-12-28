091cfd981763a17b7b8a5190b408859306bb2e39
Dave Bush <dave@davembush.com>
2025-12-28 08:14:23 -0500

    rename projects

--- apps/dms/src/app/store/risk-group/risk-group-effect-service-token.ts
+++ apps/dms/src/app/store/risk-group/risk-group-effect-service-token.ts
@@ -2,6 +2,5 @@ import { InjectionToken } from '@angular/core';
 
 import { RiskGroupEffectsService } from './risk-group-effect.service';
 
-export const riskGroupEffectsServiceToken = new InjectionToken<RiskGroupEffectsService>(
-  'RiskGroupEffectsService'
-);
+export const riskGroupEffectsServiceToken =
+  new InjectionToken<RiskGroupEffectsService>('RiskGroupEffectsService');
