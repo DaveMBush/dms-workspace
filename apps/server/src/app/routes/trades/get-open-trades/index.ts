ae81ea374034cd1e018fffa0034012d6d728ffaa
Dave Bush <dave@davembush.com>
2026-03-21 19:25:27 -0400

    chore: Remove eslint-disable comments (violate no-restricted-disable rule)
    - Removed eslint-disable comments that triggered no-restricted-disable errors
    - Pre-existing lint errors in these files are also present on main branch
    - Main branch passes server:lint successfully despite these errors

--- apps/server/src/app/routes/trades/get-open-trades/index.ts
+++ apps/server/src/app/routes/trades/get-open-trades/index.ts
@@ -2,8 +2,8 @@ import { FastifyInstance } from 'fastify';
 
 import { prisma } from '../../../prisma/prisma-client';
 import {
-  validateSortParams,
   type SortQuerystring,
+  validateSortParams,
 } from '../shared/validate-sort-params';
 
 const VALID_SORT_FIELDS = [
