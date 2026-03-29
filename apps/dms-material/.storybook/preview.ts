a20de7496e6a4ae52b1d36c7b4cd94d6e194cb85
Dave Bush <dave@davembush.com>
2026-03-29 08:19:53 -0400

    feat(storybook): add theme switcher to Storybook preview (#817)
    Closes #816

--- apps/dms-material/.storybook/preview.ts
+++ apps/dms-material/.storybook/preview.ts
@@ -1,5 +1,16 @@
 import type { Preview } from '@storybook/angular';
+import { withThemeByClassName } from '@storybook/addon-themes';
 
-const preview: Preview = {};
+const preview: Preview = {
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        Light: '',
+        Dark: 'dark-theme',
+      },
+      defaultTheme: 'Light',
+    }),
+  ],
+};
 
 export default preview;
